68f8f68ca5df84b660f003001f38d6bfb1252d4f
Humberto <humbertosegueda1997@gmail.com>
2026-04-20 20:21:45 -0600

    Feature: Agregar fecha de vencimiento (DueDate) a las tareas

--- TaskMaster.Api/Migrations/20260421021632_AddDueDateToTasks.Designer.cs
+++ TaskMaster.Api/Migrations/20260421021632_AddDueDateToTasks.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TaskMaster.Api.Data;
 
@@ -11,9 +12,11 @@ using TaskMaster.Api.Data;
 namespace TaskMaster.Api_.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260421021632_AddDueDateToTasks")]
+    partial class AddDueDateToTasks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- TaskMaster.Api/Models/TodoTask.cs
+++ TaskMaster.Api/Models/TodoTask.cs
@@ -7,4 +7,6 @@ public class TodoTask
     public string Description { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; } = DateTime.Now;
     public TaskStatus Status { get; set; } = TaskStatus.Pending;
+
+    public DateTime? DueDate { get; set; }
 }
